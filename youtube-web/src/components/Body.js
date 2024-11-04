a75a12c6c8ebace93110c0e5dd80bf2d7e25c972
RISHAB GUPTA <rishabkumar0703@gmail.com>
2024-11-04 17:43:22 +0530

    home page sidebar css fix

--- youtube-web/src/components/Body.js
+++ youtube-web/src/components/Body.js
@@ -1,11 +1,15 @@
 import SideBar from "./SideBar";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Body = () => {
+  const isSideBarMenuOpen = useSelector((store) => store.app.isSideBarMenuOpen);
   return (
     <div className="flex">
-      <SideBar />
-      <Outlet />
+      {isSideBarMenuOpen && <SideBar />}
+      <div className={`${isSideBarMenuOpen ? "w-[calc(100%-12rem)]" : "w-full"} transition-all duration-300`}>
+        <Outlet />
+      </div>  
     </div>
   )
 };
--- youtube-web/src/components/SideBar.js
+++ youtube-web/src/components/SideBar.js
@@ -1,5 +1,20 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import MovieIcon from '@mui/icons-material/Movie';
+import MusicIcon from '@mui/icons-material/MusicNote'
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import SportsCricket from '@mui/icons-material/SportsCricket';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import ShortTextIcon from '@mui/icons-material/ShortText';
+import Whatshot from '@mui/icons-material/Whatshot';
+import News from '@mui/icons-material/Newspaper';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import PodcastsIcon from '@mui/icons-material/Podcasts';
+import HistoryIcon from '@mui/icons-material/History';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
 
 const SideBar = () => {
   const isSideBarMenuOpen = useSelector((store) => store.app.isSideBarMenuOpen);
@@ -7,28 +22,68 @@ const SideBar = () => {
   if(!isSideBarMenuOpen)
     return;
   return (
-    <div className="p-5 shadow-lg w-48">
-      <ul>
-        <li>
+    <div className="p-5 shadow-lg w-48 bg-white h-screen">
+      <ul className="space-y-4 pt-4">
+        <li className="flex items-center space-x-2"> 
+          <HomeIcon />
           <Link to="/">Home</Link>
         </li>
-        <li>Shorts</li>
-        <li>Videos</li>
-        <li>Live</li>
+        <li className="flex items-center space-x-2">
+          <ShortTextIcon />
+          <span>Shorts</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <LiveTvIcon />
+          <span>Live</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <Whatshot />
+          <span>Trending</span>
+        </li>
       </ul>
-      <h1 className="font-bold pt-5">Subscriptions</h1>
-      <ul>
-        <li> Music</li>
-        <li> Sports</li>
-        <li> Gaming</li>
-        <li> Movies</li>
+
+      <h1 className="font-bold pt-5">Explore</h1>
+      <ul className="space-y-4 pt-4">
+        <li className="flex items-center space-x-2">
+          <SubscriptionsIcon />
+          <span>Subscriptions</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <SportsCricket />
+          <span>Cricket</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <MovieIcon />
+          <span>Movies</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <News />
+          <span>News</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <ShoppingBagIcon />
+          <span>Shopping</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <PodcastsIcon />
+          <span>Podcasts</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <SportsEsportsIcon />
+          <span>Gaming</span>
+        </li>
       </ul>
+
       <h1 className="font-bold pt-5">Watch Later</h1>
-      <ul>
-        <li> Music</li>
-        <li> Sports</li>
-        <li> Gaming</li>
-        <li> Movies</li>
+      <ul className="space-y-4 pt-4">
+        <li className="flex items-center space-x-2">
+          <HistoryIcon />
+          <span>History</span>
+        </li>
+        <li className="flex items-center space-x-2">
+          <ThumbUpIcon />
+          <span>Liked Videos</span>
+        </li>
       </ul>
     </div>
   )
