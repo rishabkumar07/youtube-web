8f6fafb9e2ac99f9e62fea81a231d2abdbfd1a5f
RISHAB GUPTA <rishabkumar0703@gmail.com>
2025-01-27 01:24:13 +0530

    minor fix

--- youtube-web/src/components/RecommendationVideo.js
+++ youtube-web/src/components/RecommendationVideo.js
@@ -42,27 +42,29 @@ const RecommendationVideo = ({ videoTitle, videoId }) => {
   }
 
   return (
-    <div className="w-full mt-4">
-      <h2 className="text-lg font-semibold mb-2">Recommended Videos</h2>
-      <div className="flex flex-col space-y-4">
-        {loading ? (
-          Array.from({length : 10}).map((_, index) => (
-            <div key={index} className="flex mb-4 hover:bg-gray-100 p-2 rounded-md">
-              <Skeleton width={120} height={90} className="mr-4 rounded-md" />
-              <div className="flex flex-col justify-between w-full">
-                <Skeleton height={20} width="60%" />
-                <Skeleton height={15} width="40%" />
+    recommendedVideos.length > 0 && (
+      <div className="w-full mt-4">
+        <h2 className="text-lg font-semibold mb-2">Recommended Videos</h2>
+        <div className="flex flex-col space-y-4">
+          {loading ? (
+            Array.from({length : 10}).map((_, index) => (
+              <div key={index} className="flex mb-4 hover:bg-gray-100 p-2 rounded-md">
+                <Skeleton width={120} height={90} className="mr-4 rounded-md" />
+                <div className="flex flex-col justify-between w-full">
+                  <Skeleton height={20} width="60%" />
+                  <Skeleton height={15} width="40%" />
+                </div>
               </div>
-            </div>
-          ))
-        ):(
-          recommendedVideos.map((video) => (
-            <RecommendationCard key={video.id} video={video} />
-          ))
-        )}
+            ))
+          ):(
+            recommendedVideos.map((video) => (
+              <RecommendationCard key={video.id} video={video} />
+            ))
+          )}
+        </div>
       </div>
-    </div>
-  )
+    )
+  )  
 };
 
 export default RecommendationVideo;
